f6642f141c8676d002bfcf88fc50eb817466e90d
Jerson <jersonjanke@gmail.com>
2018-07-01 15:09:49 -0300

    Tipos na typescript

--- tipos.ts
+++ tipos.ts
@@ -2,8 +2,10 @@ let mensagem: string = "Seja bem vindo!";
 let temporasFriends: number = 10;
 let estudandoAngular: boolean = true;
 
+//Array string
 let listaDeFrutas: Array<string> = ['Uva', 'Banana', 'Abacaxi'];
 let listaDeFrutas2: string[] = ['Uva', 'Banana', 'Abacaxi'];
 
+//Array number
 let notasDasProvas: Array<number> = [7.5, 8, 9];
 let notasDasProvas2: number[] = [7.5, 8, 9];
